905ae0dc30d4843c293010a4ead29fd8ac36da9a
John Nkp <john.nkp1@gmail.com>
2021-04-12 23:45:10 +1000

    minor adjustments...

--- script.js
+++ script.js
@@ -3,37 +3,52 @@ const textarea = document.getElementById('textarea')
 const randomBtn = document.getElementById('random')
 const resetBtn = document.getElementById('reset')
 const addBtn = document.getElementById('add')
+const completeBtn = document.getElementById('complete')
 
 textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
 
-    // if(e.key === 'Enter'){
-    //     setTimeout(() => {
-    //         // e.target.value = ''
-    //     }, 10)
+    if(e.key === 'Enter'){
+        setTimeout(() => {
+            // e.target.value = ''
+        }, 10)
 
-    //     randomSelect()
-    // }
+        randomSelect()
+    }
 })
 
 randomBtn.addEventListener('click',() =>{
     setTimeout(() => {
         randomSelect()
     }, 10)
-
-    
 })
 
 resetBtn.addEventListener('click', (e) => {
     createTags(e.target.value)
-    e.target.value = ''
-    textarea.innerText = '';
-    textarea.placeholder.innerText = 'Enter tasks here...'
+
+    e.target.value = '';
+    textarea.innerHTML = '';
+    tagsEl.innerHTML = '';
 })
 
 function createTags(input) {
+    
+    // addBtn.addEventListener('click', () => {
+    //     const space = " ";
+    //     const tags = input.split(space).filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    //     tagsEl.innerHTML = ''
+
+    //     tags.forEach(tag => {
+    //     const tagEl = document.createElement('span')
+    //     tagEl.classList.add('tag')
+    //     tagEl.innerText = tag
+    //     tagsEl.appendChild(tagEl)
+    //     })
+    // })
+
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
     
     tagsEl.innerHTML = ''
@@ -73,7 +88,33 @@ function randomSelect() {
 
 function pickRandomTag() {
     const tags = document.querySelectorAll('.tag')
-    return tags[Math.floor(Math.random() * tags.length)]
+    const randomTag = tags[Math.floor(Math.random() * tags.length)]
+
+    // console.log(randomTag);
+
+    // const index = tags.indexOf(randomTag);
+    // tags.splice(index, 1)
+
+    indexCheck(randomTag)
+
+    return randomTag
+
+    // completedTask(randomTag);
+
+}
+
+function indexCheck(input){
+    const tags = document.querySelectorAll('.tag');
+
+    if(tags.includes(input)){
+        console.log('yes');
+    }
+}
+
+function completedTask(input) {
+    completeBtn.addEventListener('click', () => {
+        input.style.textDecoration = "underline overline";
+    })
 }
 
 function highlightTag(tag) {
